76727afc1b5a750e594f39975cdbb3f848bed2c8
jlegrand62 <jlegrand62@gmail.com>
2018-06-26 02:44:46 +0200

    Change venv_setup.sh to pi_setup.sh Now install the virtualenv and filament_watch.

---
+++ pi_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# - Create VirtualEnv:
+virtualenv filament_watch_env
+
+# - Load it:
+. ~/filament_watch_env/bin/activate
+
+# - Make sure the venv is loaded:
+if [[ -n VIRTUAL_ENV ]]; then
+    exit 1
+
+# - Install filament_watch:
+python setup.py install
+exit 0
--- venv_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-virtualenv filament_watch_env
-. filament_watch_env/bin/activate
